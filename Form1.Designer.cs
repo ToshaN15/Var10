16af283bc0e178516988baf5f6b1945852089fbe
Андрей <@DESKTOP-QVHC0LO>
2023-04-19 11:59:30 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Var10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.predpriyatieDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.predpriyatieDataSet = new Var10.predpriyatieDataSet();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.predpriyatieDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.predpriyatieDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Список сотрудников не явившихся на осмотр";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(629, 302);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // predpriyatieDataSetBindingSource
+            // 
+            this.predpriyatieDataSetBindingSource.DataSource = this.predpriyatieDataSet;
+            this.predpriyatieDataSetBindingSource.Position = 0;
+            // 
+            // predpriyatieDataSet
+            // 
+            this.predpriyatieDataSet.DataSetName = "predpriyatieDataSet";
+            this.predpriyatieDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(516, 376);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 62);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Колличество сотрудников прошедших осмотр за период";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(361, 344);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "День (DD)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(428, 341);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(428, 376);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(356, 379);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Месяц (MM)";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(428, 407);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(65, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(361, 410);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Год (YYYY)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.predpriyatieDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.predpriyatieDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource predpriyatieDataSetBindingSource;
+        private predpriyatieDataSet predpriyatieDataSet;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Var10
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection con = null;
+        private SqlCommandBuilder sqlBuilder = null;
+        private SqlDataAdapter sqlDataAdapter = null;
+        private DataSet dataSet = null;
+        DataTable tabel;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e) // список
+        {
+
+            //tabel.Clear();
+            string connstr = @"Data Source=DESKTOP-QVHC0LO\SQLEXPRESS;Initial Catalog = predpriyatie; Integrated Security = True";
+            con = new SqlConnection(connstr);
+            con.Open();
+            sqlDataAdapter = new SqlDataAdapter("SELECT fam, namee, otche FROM sotrudnik LEFT JOIN MedKarta ON sotrudnik.Id = IdSotr LEFT JOIN DataOsmotra ON MedKarta.Id = Idkart where Result = 'нет';", con);
+            tabel = new DataTable();
+            sqlDataAdapter.Fill(tabel);
+
+            dataGridView1.DataSource = tabel;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string connstr = @"Data Source=DESKTOP-QVHC0LO\SQLEXPRESS;Initial Catalog = predpriyatie; Integrated Security = True";
+            //SqlConnection con = new SqlConnection(connstr);
+        }
+
+        private void button2_Click(object sender, EventArgs e) //колво
+        {
+            string connstr = @"Data Source=DESKTOP-QVHC0LO\SQLEXPRESS;Initial Catalog = predpriyatie; Integrated Security = True";
+            con = new SqlConnection(connstr);
+            con.Open();
+            string datatime = textBox1.Text + "/" + textBox2.Text + "/" + textBox3.Text;
+            label1.Text = datatime;
+            sqlDataAdapter = new SqlDataAdapter("SELECT COUNT(*) FROM DataOsmotra where Result != 'нет' AND Dat = '" + datatime + "';", con);
+            tabel = new DataTable();
+            sqlDataAdapter.Fill(tabel);
+
+            dataGridView1.DataSource = tabel;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
